fb78a89720f09b641f08ea1db0e4fa61830887e5
alansoto31 <soto.alan@hotmail.com>
2021-01-26 14:34:08 -0600

    Add user's sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  root 'fishings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   resources :users
   resources :fishings
   resources :priorities
