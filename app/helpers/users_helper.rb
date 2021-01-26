652bc414a731f6142ce94f85b1c81f88fd81bbd5
alansoto31 <soto.alan@hotmail.com>
2021-01-26 14:25:28 -0600

    User registration

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def user_errors
+    return unless @user.errors.any?
+
+    @user.errors.full_messages.each do |msg|
+      if msg.include?('Name')
+        @uclas1 = 'border border-danger'
+      end
+    end
+  end
+
 end
