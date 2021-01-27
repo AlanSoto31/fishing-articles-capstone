e86f303a784bce1bc298f4e6c52a665d4ad0093d
alansoto31 <soto.alan@hotmail.com>
2021-01-27 10:23:49 -0600

    Add nav-bar funcionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+  def nav_bar_buttons
+    unless user_sign_in?
+      @su_button = link_to 'Sign Up', new_user_path, class: 'btn btn-info text-white'
+      @si_button = link_to 'Log in', login_path, class: 'btn btn-info text-white'
+      @welcome = 'Connect first please'
+    else
+      @so_button = link_to 'Log out', logout_path, class: 'btn btn-info text-white '
+      @welcome = "Welcome #{current_user.name}!"
+    end
+  end
 end
