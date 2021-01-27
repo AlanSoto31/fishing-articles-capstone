64e82c2ed41f535bde92cdd6ba5068c0f89505e3
alansoto31 <soto.alan@hotmail.com>
2021-01-27 14:20:25 -0600

    Refactor vote button

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,6 +1,8 @@
 module VotesHelper
   def make_vote(f_id)
-    return if current_user.votes.find_by(fishing_id: f_id)
-    @vote = link_to 'Vote', votes_path(fishing_id: f_id), method: :post, class: 'btn btn-info text-white'
+    if user_sign_in?
+      return if current_user.votes.find_by(fishing_id: f_id)
+      @vote = link_to 'Vote', votes_path(fishing_id: f_id), method: :post, class: 'btn btn-info text-white'
+    end
   end
 end
