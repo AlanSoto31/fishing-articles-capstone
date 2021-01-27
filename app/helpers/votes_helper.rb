5331ab2415caa40da0ac8af739b810eca584202b
alansoto31 <soto.alan@hotmail.com>
2021-01-27 14:17:15 -0600

    Create vote button

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,6 @@
 module VotesHelper
+  def make_vote(f_id)
+    return if current_user.votes.find_by(fishing_id: f_id)
+    @vote = link_to 'Vote', votes_path(fishing_id: f_id), method: :post, class: 'btn btn-info text-white'
+  end
 end
