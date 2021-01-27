cca4d2addb0a1a29fd0adf75e147ad21fb3817d9
alansoto31 <soto.alan@hotmail.com>
2021-01-27 16:49:56 -0600

    Create bridge table between Fishing and Category models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_223021) do
+ActiveRecord::Schema.define(version: 2021_01_27_224657) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "priority"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categorizes", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "fishing_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_categorizes_on_category_id"
+    t.index ["fishing_id"], name: "index_categorizes_on_fishing_id"
   end
 
   create_table "fishings", force: :cascade do |t|
@@ -45,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_223021) do
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
+  add_foreign_key "categorizes", "categories"
+  add_foreign_key "categorizes", "fishings"
   add_foreign_key "fishings", "users", column: "author_id"
   add_foreign_key "votes", "fishings"
   add_foreign_key "votes", "users"
