250be974c2ad3eed493761e7512d15d5258e5df5
alansoto31 <soto.alan@hotmail.com>
2021-01-27 17:00:59 -0600

    Add bridge table associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :categorizes
+  has_many :fishings, through: :categorizes
 end
--- app/models/fishing.rb
+++ app/models/fishing.rb
@@ -1,6 +1,8 @@
 class Fishing < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :votes
+  has_many :categorizes
+  has_many :categories, through: :categorizes
 
   scope :order_by_votes, -> { order(votes_count: :desc) } 
 
