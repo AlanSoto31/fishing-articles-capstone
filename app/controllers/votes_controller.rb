317aca80d38266a4b9ee1a54170808fc74c6a554
alansoto31 <soto.alan@hotmail.com>
2021-01-27 11:43:11 -0600

    Create Votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,21 @@
 class VotesController < ApplicationController
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = current_user.votes.build(vote_params)
+    if @vote.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def vote_params
+    params.permit(:fishing_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 3..15}
 
   has_many :fishings, foreign_key: :author_id
+  has_many :votes
 end
