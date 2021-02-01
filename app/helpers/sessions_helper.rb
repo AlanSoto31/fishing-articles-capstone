0a180fec2f489c521fbdad863064eaafb14d8d65
alansoto31 <soto.alan@hotmail.com>
2021-02-01 15:43:42 -0600

    Add style to nav-bar

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,12 @@
 module SessionsHelper
   def nav_bar_buttons
     unless user_sign_in?
-      @su_button = link_to 'Sign Up', new_user_path, class: 'btn btn-info text-white'
-      @si_button = link_to 'Log in', login_path, class: 'btn btn-info text-white'
+      @su_button = link_to 'REGISTER', new_user_path, class: 'text-decoration-none'
+      @si_button = link_to 'LOGIN', login_path, class: 'text-decoration-none'
       @welcome = 'Connect first please'
+      @pipe= "|"
     else
-      @so_button = link_to 'Log out', logout_path, class: 'btn btn-info text-white '
+      @so_button = link_to 'LOGOUT', logout_path, class: 'text-decoration-none'
       @welcome = "Welcome #{current_user.name}!"
     end
   end
