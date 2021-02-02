5a871aa28d58831b808d9922e41cc27b404e1fb0
alansoto31 <soto.alan@hotmail.com>
2021-02-01 21:42:13 -0600

    Add style to home-page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,9 @@ module SessionsHelper
     unless user_sign_in?
       @su_button = link_to 'REGISTER', new_user_path, class: 'text-decoration-none'
       @si_button = link_to 'LOGIN', login_path, class: 'text-decoration-none'
-      @welcome = 'Connect first please'
-      @pipe= "|"
     else
+      @article = link_to 'ARTICLE', new_fishing_path, class: 'text-decoration-none' 
       @so_button = link_to 'LOGOUT', logout_path, class: 'text-decoration-none'
-      @welcome = "Welcome #{current_user.name}!"
     end
   end
 end
